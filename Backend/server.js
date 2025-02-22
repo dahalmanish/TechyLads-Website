4de5fb25c648938a2466e018b35aa172cc592942
Manish Dahal <manishdahal445@gmail.com>
2025-02-22 21:01:54 +0545

    Last

--- Backend/server.js
+++ Backend/server.js
@@ -19,8 +19,6 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
